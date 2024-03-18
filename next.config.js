69028aa631a2f9dca31e1061a28920fa84f3c654
rahmatalmubarak <79588035+rahmatalmubarak@users.noreply.github.com>
2024-03-18 08:53:16 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
