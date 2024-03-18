6246dc409c25d2277b3383768af664aa3b4f7d6e
Rahmat Almubarak <rahmatalmubarak35@gmail.com>
2024-03-18 11:10:11 +0700

    20

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,22 +1,22 @@
 export const Social_Icons = [
   {
     link: "https://github.com/rahmatalmubarak",
-    image: "/Github.svg",
+    image: "Github.svg",
     alt: "Github",
   },
   {
     link: "",
-    image: "/Instagram.svg",
+    image: "Instagram.svg",
     alt: "Instagram",
   },
   {
     link: "https://www.linkedin.com/in/rahmat-almubarak-1b9595225/",
-    image: "/LinkedIn.svg",
+    image: "LinkedIn.svg",
     alt: "LinkedIn",
   },
   {
     link: "",
-    image: "/Twitter.svg",
+    image: "Twitter.svg",
     alt: "Twitter",
   },
 ];
