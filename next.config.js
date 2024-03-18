cf4aa2c8c86a58b66ddd4d3244906579d22bd8be
Rahmat Almubarak <rahmatalmubarak35@gmail.com>
2024-03-18 09:09:10 +0700

    6

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export", // <=== enables static exports
     reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
