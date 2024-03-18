b8d9832657110e2591dbb4c12a23d232b6103ab1
Rahmat Almubarak <rahmatalmubarak35@gmail.com>
2024-03-18 10:12:17 +0700

    11

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-
-    // Add basePath
-    basePath: '/github-pages',
+    output: "export", // <=== enables static exports
 };
 
 module.exports = nextConfig;
